6a2390cd04a09725b3db3fe4c56694dba9b77ca2
Jurandi Junior <jurandijr20@gmail.com>
2025-02-15 21:43:36 -0300

    Generics PARTE 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿namespace POO_CSharp_P19
+﻿using POO_CSharp_P19.Service;
+
+namespace POO_CSharp_P19
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+
+            Console.WriteLine("First: " + printService.First());
         }
     }
 }
